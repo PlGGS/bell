354a68ed48f011a94dd3ef26a03b2f3950381fbc
PlGGS <bbor0422@gmail.com>
2023-04-11 19:46:52 -0500

    Added sorting algorithm implementations

---
+++ hw/hw1/hw1/Sort.swift
@@ -0,0 +1,95 @@
+//
+//  Sort.swift
+//  hw1
+//
+//  Created by Blake Boris on 4/11/23.
+//
+
+import Foundation
+
+struct Sort {
+    static func insertion<T: Comparable>(array: inout [T]) -> Void { return insertionSort(&array) };
+    static func selection<T: Comparable>(array: inout [T]) -> Void { return selectionSort(&array) };
+    static func quick<T: Comparable>(array: inout [T]) -> Void { return quickSort(&array) };
+    static func merge<T: Comparable>(array: inout [T]) -> Void { return mergeSort(&array) };
+}
+
+func insertionSort<T: Comparable>(_ array: inout [T]) {
+    guard array.count > 1 else { return }
+    
+    for index in 1..<array.count {
+        var currentIndex = index
+        while currentIndex > 0 && array[currentIndex] < array[currentIndex - 1] {
+            array.swapAt(currentIndex - 1, currentIndex)
+            currentIndex -= 1
+        }
+    }
+}
+
+func selectionSort<T: Comparable>(_ array: inout [T]) {
+    guard array.count > 1 else { return }
+
+    for index in 0..<array.count - 1 {
+        var minIndex = index
+        for innerIndex in index + 1..<array.count {
+            if array[innerIndex] < array[minIndex] {
+                minIndex = innerIndex
+            }
+        }
+        if index != minIndex {
+            array.swapAt(index, minIndex)
+        }
+    }
+}
+
+func quickSort<T: Comparable>(_ array: inout [T]) {
+    guard array.count > 1 else { return } // check array length
+
+    let pivot = array.remove(at: 0)
+    var left: [T] = []
+    var right: [T] = []
+    for element in array {
+        if element < pivot {
+            left.append(element)
+        } else {
+            right.append(element)
+        }
+    }
+    quickSort(&left)
+    quickSort(&right)
+    array = left + [pivot] + right
+}
+
+func mergeSort<T: Comparable>(_ array: inout [T]) {
+    if array.count > 1 {
+        let middleIndex = array.count / 2;
+        var leftArray = Array(array[..<middleIndex]);
+        var rightArray = Array(array[middleIndex...]);
+
+        mergeSort(&leftArray);
+        mergeSort(&rightArray);
+
+        array = merge(left: leftArray, right: rightArray);
+    }
+}
+
+func merge<T: Comparable>(left: [T], right: [T]) -> [T] {
+    var merged = [T]();
+    var leftIndex = 0;
+    var rightIndex = 0;
+
+    while leftIndex < left.count && rightIndex < right.count {
+        if left[leftIndex] < right[rightIndex] {
+            merged.append(left[leftIndex]);
+            leftIndex += 1;
+        } else {
+            merged.append(right[rightIndex]);
+            rightIndex += 1;
+        }
+    }
+
+    merged += left[leftIndex...];
+    merged += right[rightIndex...];
+    
+    return merged;
+}
--- hw/hw1/hw1/ViewController.swift
+++ hw/hw1/hw1/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad();
+        
+        var ints = [6, 5, 7, 4, 7, 1, 8, 2, 2, 8, 4, 8, 7, 3, 8, 1, 8, 6, 10, 2, 3, 8, 8];
+        Sort.merge(array: &ints)
+        print(ints);
     }
 }
 
+func getRandomInt(_ from: UInt32, _ to: UInt32) -> Int {
+    return Int(arc4random_uniform(to - from + 1)) + Int(from);
+}
