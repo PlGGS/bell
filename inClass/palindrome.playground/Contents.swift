8ebbb4c3f7185bc79b1c1cd5cd41a841496c58a1
PlGGS <bbor0422@gmail.com>
2023-04-17 22:47:22 -0500

    Added palindrome exercise from the lecture for week 4

--- inClass/palindrome.playground/Contents.swift
+++ inClass/palindrome.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "racecar"
+
+print(isPalindromeHalf(greeting));
+print(isPalindromeFull(greeting));
+
+func isPalindromeHalf(_ str: String) -> Bool {
+    let prefix = str.prefix(str.count / 2);
+    let suffix = str.suffix(str.count / 2);
+    
+    print(type(of: prefix));
+    print(type(of: suffix));
+    
+    return String(prefix) == String(suffix.reversed());
+}
+
+func isPalindromeFull(_ str: String) -> Bool {
+    return str == String(str.reversed());
+}
